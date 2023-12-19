f8cfb39c6b58e40d23e83d68c635f7e232ea91a4
Oleksis Fraga Menéndez <oleksis.fraga@gmail.com>
2023-12-19 11:58:04 -0500

    [ohmyposh] Install Monaspace, Firacode in `.local` if no exists

--- .bashrc.d/80_ohmyposh.sh
+++ .bashrc.d/80_ohmyposh.sh
@@ -4,3 +4,11 @@ if [ ! -f ~/.poshthemes/microverse-power.omp.json ]; then
     curl https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/microverse-power.omp.json -o ~/.poshthemes/microverse-power.omp.json
 fi
 eval "$(oh-my-posh init bash --config ~/.poshthemes/microverse-power.omp.json)"
+
+if [ ! -d ~/.local/share/fonts/monaspicear-nerd-font ]; then
+    oh-my-posh font install Monaspace
+fi
+
+if [ ! -d ~/.local/share/fonts/firacode-nerd-font ]; then
+    oh-my-posh font install FiraCode
+fi
