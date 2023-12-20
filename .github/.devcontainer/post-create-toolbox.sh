343c91626e1747042014af84724b7d0c934748c9
Oleksis Fraga Menéndez <oleksis.fraga@gmail.com>
2023-12-19 22:03:42 -0500

    Setting .bashrc.d if not exist

--- .github/.devcontainer/post-create-toolbox.sh
+++ .github/.devcontainer/post-create-toolbox.sh
@@ -4,10 +4,12 @@ curl -s https://ohmyposh.dev/install.sh | bash -s
 curl https://pyenv.run | bash
 pre-commit install
 # Setting bashrc.d
-TOOLBOX_DIR="/workspaces/toolbox"
-ln -s $TOOLBOX_DIR/.bashrc.d ~/.bashrc.d
-. $TOOLBOX_DIR/.bashrc
-unset $TOOLBOX_DIR
+if [ ! -d ~/.bashrc.d ]; then
+    TOOLBOX_DIR="/workspaces/toolbox"
+    ln -s $TOOLBOX_DIR/.bashrc.d ~/.bashrc.d
+    . $TOOLBOX_DIR/.bashrc
+    unset $TOOLBOX_DIR
+fi
 ## [Pyenv](https://github.com/pyenv/pyenv/blob/master/COMMANDS.md#pyenv-install)
 if [ ! -d ~/.pyenv/versions/3.12.1 ]; then
     env PYTHON_CONFIGURE_OPTS="--enable-shared" ~/.pyenv/bin/pyenv install ${PYTHON_LAST}
