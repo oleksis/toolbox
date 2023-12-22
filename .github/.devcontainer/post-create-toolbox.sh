cd3af7a50d2385cc6dd918d6ce71db7c7324825b
Oleksis Fraga Menéndez <oleksis.fraga@gmail.com>
2023-12-21 23:48:01 -0500

    [devcontainer] Pre-build Oh-My-Posh and Pyenv

--- .github/.devcontainer/post-create-toolbox.sh
+++ .github/.devcontainer/post-create-toolbox.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-curl -s https://ohmyposh.dev/install.sh | bash -s
-curl https://pyenv.run | bash
 pre-commit install
 # Setting bashrc.d
 if [ ! -d ~/.bashrc.d ]; then
@@ -9,8 +7,4 @@ if [ ! -d ~/.bashrc.d ]; then
     ln -s $TOOLBOX_DIR/.bashrc.d ~/.bashrc.d
     . $TOOLBOX_DIR/.bashrc
     unset $TOOLBOX_DIR
-fi
-## [Pyenv](https://github.com/pyenv/pyenv/blob/master/COMMANDS.md#pyenv-install)
-if [ ! -d ~/.pyenv/versions/3.12.1 ]; then
-    env PYTHON_CONFIGURE_OPTS="--enable-shared" ~/.pyenv/bin/pyenv install ${PYTHON_LAST}
 fi
