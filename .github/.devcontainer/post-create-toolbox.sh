ee91c8a639f43a569df7f6a29a282b12dd500b64
Oleksis Fraga Menéndez <oleksis.fraga@gmail.com>
2023-12-22 02:16:44 -0500

    Update post create script Use '$HOME'

--- .github/.devcontainer/post-create-toolbox.sh
+++ .github/.devcontainer/post-create-toolbox.sh
@@ -2,9 +2,9 @@
 
 pre-commit install
 # Setting bashrc.d
-if [ ! -d ~/.bashrc.d ]; then
+if [ ! -d $HOME/.bashrc.d ]; then
     TOOLBOX_DIR="/workspaces/toolbox"
-    ln -s $TOOLBOX_DIR/.bashrc.d ~/.bashrc.d
+    ln -s $TOOLBOX_DIR/.bashrc.d $HOME/.bashrc.d
     . $TOOLBOX_DIR/.bashrc
     unset $TOOLBOX_DIR
 fi
